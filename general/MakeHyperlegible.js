65d6b55eb73eb9588b18843854c9fee88876907d
Murray Inman <murray.inman@riosalado.edu>
2024-07-09 09:15:27 -0700

    Code for a "Make Hyperlegible" bookmarklet.

---
+++ general/MakeHyperlegible.js
@@ -0,0 +1,23 @@
+const hButton = document.createElement("button");
+hButton.innerText = "H";
+hButton.type = "button";
+hButton.ariaLabel = "Toggle Atkinson Hyperlegible font";
+hButton.id = "hButton";
+
+const linkMHStylesheet = document.createElement("link");
+linkMHStylesheet.rel = "stylesheet";
+linkMHStylesheet.href = "https://github.com/murrayinman/userscripts/raw/main/general/MakeHyperlegible.css";
+document.head.appendChild(linkMHStylesheet);
+
+let isFontOn = false;
+function toggleFont() {
+  if (isFontOn) {
+    document.body.classList.remove("atkinson-hyperlegible");
+  } else {
+    document.body.classList.add("atkinson-hyperlegible");
+  }
+  isFontOn = !isFontOn;
+}
+
+hButton.addEventListener("click", toggleFont);
+document.body.appendChild(hButton);
